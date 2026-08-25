9f1e3feddc8ec3f7ad9bace7cc9ff7e6273aebdb
hongsaoleang <hongsaoleang@gmail.com>
2026-08-25 22:30:15 +0700

    d

--- life_sync_app/lib/main.dart
+++ life_sync_app/lib/main.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:life_sync_app/features/finance/presentation/pages/finance/finance_page.dart';
-import 'package:life_sync_app/features/finance/presentation/pages/finance/financial_analysis_page.dart';
-import 'package:life_sync_app/features/goals/presentation/pages/goals_page.dart';
-import 'package:life_sync_app/features/home/presentation/pages/home_page.dart';
-
-
-import 'core/constants/app_info.dart';
-import 'core/theme/app_theme.dart';
-=======
 import 'package:life_sync_app/views/authentication/sign_in_screen.dart';
 
->>>>>>> f41debb (done all screens)
 
 void main() {
   runApp(
@@ -26,13 +15,7 @@ class LifeSyncApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-<<<<<<< HEAD
-      title: AppInfo.appName,
-      theme: AppTheme.light,
-      home: const HomePage(),
-=======
       home: SignInScreen(),
->>>>>>> f41debb (done all screens)
     );
   }
 }
